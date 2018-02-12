5c24fa78fd197c71965043c44eb6c5f029f629c6
Linda Tong <linda@linglinglife.com>
2018-02-12 12:18:57 +1100

    removed log/development.log, did sign up page with p/w digest

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
+
+  root to: 'pages#home'
+  get '/pages/home'
+  
   resources :games
   resources :stats
   resources :users
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
