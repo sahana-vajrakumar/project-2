acaa8bb9f1daa0ad8b92fe8f9c4ce66dc7dfde34
tnpriyanka12 <tnpriyanka12@gmail.com>
2018-02-13 18:18:20 +1100

    Game logic-timer set, restart working

--- app/assets/javascripts-custom/english.js
+++ app/assets/javascripts-custom/english.js
@@ -1,137 +1,188 @@
 
-let h1 = $('<h1></h1>');
-let container = $('<div></div>').attr('id' , 'container');
-let game = $('<div></div>').attr('id' , 'main');
-let score = $('<p></p>').attr('id' , 'score');
-let turns = $('<p></p>').attr('id' , 'turns');
-$('body').append(container);
-$('#container').append(game);
-$('#container').append(score);
-$('#container').append(turns);
+//Actual English Game
+  let h1 = $('<h1></h1>');
+  let container = $('<div></div>').attr('id' , 'container');
+  let game = $('<div></div>').attr('id' , 'main');
+  let score = $('<p></p>').attr('id' , 'score');
+  let turns = $('<p></p>').attr('id' , 'turns');
+  let timer = $('<span></span>').attr('id' , 'timer');
+
+  //Some more variables
+  let scores = 0;
+  let selectedLetter;
+  let count = 3;
+  let timerMax = 6;
+  let timeoutValue = timerMax;
+  let span = $('<span></span>');
+
+//JQuery Variables
+  $('body').append(container);
+  $('#container').append(game);
+  $('#container').append(score);
+  $('#container').append(turns);
+  $('.displayGameContols').append(timer);
+
 document.getElementById("container").style.border = "thick solid black";
 
+//When Document is ready!
 $(document).ready(function(){
-loadGame();
+
+  loadGame();
+  checkRestart();
 
 
-function loadGame() {
-
-  var button = $('#startGame');
-  //
-  // console.log('button', $('#startGame'));
-  // $('#startGame').click(() => alert('clicked!'));
-
-  // var button =  $('<button></button>');
-  // button.html ('Start Game');
-  // main.append(button);
-  
-  var main = $('#main')
-  var rules = $('<p></p>')
-  rules.html('Letters will fall... Press the correct key to knock it away before it hits the ground\nHit the right letter as soon as you see it on screen:');
-  main.append(rules);
-  button.click(function() {
-    $('#main').html('');
-    // playGame();
-    askQuestion();
-});
-}
-let scores = 0;
-let selectedLetter;
-let count = 3;
-let span = $('<span></span>');
-
-
-const askQuestion = function () {
-  let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  let index = Math.floor(Math.random() * letters.length);
-  selectedLetter = letters[index];
-  span.html(`${selectedLetter}`).addClass('displayedLetter');
-
-  $('#main').append(span);
-  console.log(`Chose letter: ${selectedLetter}`);
-
-  startAnimation();
-};
-
-
-const tryAnswer = function ( event ) {
-  console.log(event.key);
-  if(selectedLetter === event.key){
-    span.html('');
-    $('#main').append(span);
-    scores += 1;
-    $('#score').html(`Scores:${scores}`)
-    $('#turns').html(`No of tries:${count}`)
-    console.log('correct!');
-    askQuestion();
-  }else{
-    count -= 1;
-    $('#turns').html(`No of tries:${count}`)
-    if(count === 0){
-    console.log('No match');
-    span.html('Game Over');
-    console.log('score is ', scores);
-
-    $('#main').append(span);
+  function checkRestart(){
+    $('#restartGame').click(function() {
+      console.log('restart game');
+      $(document).bind('keypress', function(event ) {
+        console.log('enable default');
+      });
+      loadGame();
+      askQuestion();
+      initializeTimer();
+    });
   }
-  }
-};
-$(document).keypress(tryAnswer);
+  function loadGame() {
+     scores = 0;
+     count = 3;
+     timerMax = 6;
+     timeoutValue = timerMax;
+    var button = $('#startGame');
+    var main = $('#main')
+    var rules = $('<p></p>')
 
-
-const startAnimation = function() {
-
-  console.log('Anim start');
-  var anim = anime({
-  targets: '.displayedLetter',
-  height: '80px',
-  width: '80px',
-  translateY: '270',
-  borderRadius: '50%',
-  rotate: '360',
-  duration: '5000',
-  loop: '10'
+    //Rules of the game displayed , appended to main
+    rules.html('Letters will fall... Press the correct key to knock it away before it hits the ground\nHit the right letter as soon as you see it on screen:');
+    main.append(rules);
+    button.click(function() {
+      $('#main').html('');
+      // playGame();
+      askQuestion();
+      initializeTimer();
   });
-  anim.play();
+}//function LoadGame
 
+
+const initializeTimer = function (){
+  console.log('timer init');
+  timeoutValue = timerMax;
+  timer.html(timeoutValue);
+  console.log('timer done', timeoutValue);
+
+  var timerRunning = setInterval(function(){
+    timer.html(`    ${timeoutValue} `);
+    if(timeoutValue === 0 )
+    {
+      clearInterval(timerRunning);
+      gameOver();
+      return;
+    }
+    console.log(`${timeoutValue--}`);
+  }, 1000);
+
+  }
+
+
+
+  const askQuestion = function () {
+    let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    let index = Math.floor(Math.random() * letters.length);
+    selectedLetter = letters[index];
+    span.html(`${selectedLetter}`).addClass('displayedLetter');
+
+    $('#main').append(span);
+    console.log(`Chose letter: ${selectedLetter}`);
+
+    // startAnimation();
+  }; //askQuestion
+
+
+  const tryAnswer = function ( event ) {
+    if(count <=0 ){
+      gameOver();
+      return;
+    }
+    debugger;
+    console.log(event.key);
+    if(selectedLetter === event.key){
+      span.html('');
+      $('#main').append(span);
+      scores += 1;
+      $('#score').html(`Scores:${scores}`)
+      $('#turns').html(`No of tries:${count}`)
+      askQuestion();
+    }else{
+      count -= 1;
+      $('#turns').html(`No of tries:${count}`)
+      if(count === 0){
+      span.html('Game Over');
+      console.log('score is ', scores);
+      // gameOver(scores);
+
+      $('#main').append(span);
+    }
+    }
+  };
+
+  //When user presses key
+  $(document).keypress(tryAnswer);
+
+
+  const sendAjaxRequest = function( curr_score ) {
+  //Do an AJAX request when game ends
+  //Send all the game stats to stat controller
+  $.ajax({
+    url: '/stats',
+    method: 'post',
+    data: {
+      score: curr_score,
+      game_id: gameID,
+      accuracy: 20,
+      average_time: 1.5
+    }
+  })
+  .done(function (data) {
+    console.log(data);
+    // Once successful request is done, redirect user to the Profile page
+  })
+  .fail(function () {
+    console.log('no good!');
+  });
 
 }
 
-// h1.html('Actual Game code here');
-// $('.game').append(h1);
-// //Assuming game is done and score is generated (Rounded random score between 1 -100)
-// //On 'pry c' ->  Game.first.users.first.stats
-// let rand_score = Math.round (Math.random() * 100);
-// let h2 = $('<h2></h2>');
-// h2.html(rand_score);
-// $('body').append(h2);
-// score is available now ->
-// User has option of Save Game | Return to Game
-// create to new 'Stat' ->
-// Grab User id, current game-id
-//When User clicks 'Save Game'
 
-
-
-
-
-
-$.ajax({
-  url: '/stats',
-  method: 'post',
-  data: {
-    score: scores,
-    game_id: gameID,
-    accuracy: 20,
-    average_time: 1.5
+  //Game Over function - called once game is over
+  const gameOver = function ( score ) {
+    count = 0;
+    $(document).unbind('keypress', function(event ) {
+      console.log('prevent default');
+    });
+    const finalScore = score;
+    $('#score').html(`FINAL SCORE: ${scores}`);
+    span.html('Game Over');
   }
-})
-.done(function (data) {
-  console.log(data);
-  // Once successful request is done, redirect user to the Profile page
-})
-.fail(function () {
-  console.log('no good!');
-});
 
+
+
+
+
+
+
+
+  //Animation stuff
+  const startAnimation = function() {
+    var anim = anime({
+    targets: '.displayedLetter',
+    height: '80px',
+    width: '80px',
+    translateY: '270',
+    borderRadius: '50%',
+    rotate: '360',
+    duration: '5000',
+    loop: '10'
+    });
+    anim.play();
+
+  }
 });
